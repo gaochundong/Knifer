ef89413cda548d468f3bd222084db8d9e1c24cb6
Dennis Gao <gaochundong@hotmail.com>
2012-03-17 21:19:11 +0800

    add two new projects, rmdir and addtext, but not be implemented.

---
+++ Gimela.Toolkit.CommandLines.RemoveDirectory/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gimela.Toolkit.CommandLines.RemoveDirectory")]
+[assembly: AssemblyDescription("Gimela.Toolkit.CommandLines.RemoveDirectory")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Chundong Gao")]
+[assembly: AssemblyProduct("Gimela Toolkit")]
+[assembly: AssemblyCopyright("Copyright © 2011-2012 Chundong Gao. All rights reserved.")]
+[assembly: AssemblyTrademark("Gimela")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("451e7bef-d507-430c-b837-a9f0e2999ed8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
